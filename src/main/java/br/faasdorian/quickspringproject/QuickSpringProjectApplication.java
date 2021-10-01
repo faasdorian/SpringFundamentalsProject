6e63d20acfeb3d679d62a65edfa17ab13b0e01c7
faasdorian <faasdorian@gmail.com>
2021-09-30 21:09:47 -0300

    Initial commit

---
+++ src/main/java/br/faasdorian/quickspringproject/QuickSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package br.faasdorian.quickspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/faasdorian/quickspringproject/QuickSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faasdorian.quickspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
